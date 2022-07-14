ae60d9675fc6382960ad503a7fb077a0176d113b
Felix Klauke <felix@klauke-enterprises.com>
2022-07-14 13:25:38 +0200

    Remove test record

--- terraform/tomaryrpg.de.tf
+++ terraform/tomaryrpg.de.tf
@@ -1,9 +1,3 @@
 data "cloudflare_zone" "tomaryrpg-de" {
   name = "tomaryrpg.de"
 }
-
-resource "cloudflare_record" "test" {
-  name    = "test"
-  type    = "A"
-  zone_id = data.cloudflare_zone.tomaryrpg-de.id
-}
